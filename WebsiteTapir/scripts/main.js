83dcfa796c775f3c5cfd38875450021adc5763d0
Anthony Macchia <antmacchia15@gmail.com>
2018-10-10 19:57:46 -0400

    Added sample of reading usernames and passwords from database

--- WebsiteTapir/scripts/main.js
+++ WebsiteTapir/scripts/main.js
@@ -1,7 +1,11 @@
 function addUser() {
+    // Reference to db
     var database = firebase.database();
 
+    // Reference to users path of db
     var usersRef = database.ref('users');
+
+    // Example of data
     var data = {
         username: "amacchia",
         password: "goodpassword",
@@ -9,5 +13,23 @@ function addUser() {
         petName: "Bobo"
     }
 
+    // Write user to db
     usersRef.push(data);
+
+    // Read from db
+    usersRef.on('value', function(data) {
+        // The data returned is a js object of users
+        var users = data.val();
+
+        // An array of the keys to the user acounts
+        var keys = Object.keys(users);
+
+        // Use the keys to access the user objects
+        keys.forEach(element => {
+            var userObject = users[element];
+            console.log(userObject.username);
+            console.log(userObject.password);
+        });
+
+    });
 }
