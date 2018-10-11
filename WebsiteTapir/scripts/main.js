7c9bbd68cc7b680febceb21b1835df5f05e8ac81
JasonRrowan <rennaj4@students.rowan.edu>
2018-10-11 18:29:17 -0400

    Add files via upload

--- WebsiteTapir/scripts/main.js
+++ WebsiteTapir/scripts/main.js
@@ -1,13 +1,29 @@
+@@ -1,13 +1,35 @@
 function addUser() {
+    // Reference to db
     var database = firebase.database();
-
+     // Reference to users path of db
     var usersRef = database.ref('users');
+     // Example of data
     var data = {
         username: document.getElementById('regusername').value,
         password: document.getElementById('regpassword').value,
         petHealth: 100,
         petName: document.getElementById('regpetname').value
     }
-
+     // Write user to db
     usersRef.push(data);
-}
+     // Read from db
+    usersRef.on('value', function(data) {
+        // The data returned is a js object of users
+        var users = data.val();
+         // An array of the keys to the user acounts
+        var keys = Object.keys(users);
+         // Use the keys to access the user objects
+        keys.forEach(element => {
+            var userObject = users[element];
+            console.log(userObject.username);
+            console.log(userObject.password);
+        });
+     });
+} 
